efef42391d940a20ad84f61e2fa426acf023a702
orih <orih@Ubuntu.myguest.virtualbox.org>
2023-03-05 12:14:17 +0200

    working faker task

--- consumer.py
+++ consumer.py
@@ -1,6 +1,13 @@
 import pika
+import time
+
+def callback(ch, method, properties, body):
+    time.sleep(0.1)
+    print(f'Saving message to DB : {body.decode("utf-8")}')
+
 connection_parameters = pika.ConnectionParameters(host='localhost')
-with pika.BlockingConnection(connection_parameters) as connection:
+
+with pika.BlockingConnection(connection_parameters) as connection: 
     channel = connection.channel()
-    method, properties, body = channel.basic_get(queue='helloWorldQueue',auto_ack=True)
-    print(f'Got message: {body}')
+    channel.basic_consume(queue='fakerQueue',on_message_callback=callback,auto_ack=True)
+    channel.start_consuming()
--- publisher.py
+++ publisher.py
@@ -1,7 +1,14 @@
 import pika
+from faker import Faker
 
+fake = Faker('iw_IL')
 connection_parameters = pika.ConnectionParameters(host='localhost')
+
 with pika.BlockingConnection(connection_parameters) as connection:
     channel = connection.channel()
-    channel.queue_declare(queue='helloWorldQueue')
-    channel.basic_publish(exchange='',routing_key='helloWorldQueue',body=b'Hello World!')
+    channel.queue_declare(queue='fakerQueue')
+    for i in range(100):
+        person = fake.name()
+        location = fake.city()
+        msg = f'{person} is in city {location}'
+        channel.basic_publish(exchange='',routing_key='fakerQueue',body= msg)
