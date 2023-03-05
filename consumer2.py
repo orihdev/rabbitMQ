673f2b91589bbd0400b1a05566293bb48d26c172
orih <orih@Ubuntu.myguest.virtualbox.org>
2023-03-05 14:47:45 +0200

    or haner works

--- consumer2.py
+++ consumer2.py
@@ -3,7 +3,8 @@ import time
 
 def callback(ch, method, properties, body):
     time.sleep(0.1)
-    print(f'Saving message to DB : {body.decode("utf-8")}')
+    if "city רנה רוא" in body.decode("utf-8") :
+        print(f'{body.decode("utf-8")}')
 
 connection_parameters = pika.ConnectionParameters(host='localhost')
 
@@ -11,7 +12,7 @@ connection_parameters = pika.ConnectionParameters(host='localhost')
 with pika.BlockingConnection(connection_parameters) as connection: 
     channel = connection.channel()
     channel.exchange_declare(exchange='FanoutPractice',exchange_type='fanout')
-    channel.queue_declare(queue='fanoutTask')
-    channel.queue_bind(exchange='FanoutPractice',queue='fanoutTask')
-    channel.basic_consume(queue='fanoutTask',on_message_callback=callback,auto_ack=True)
+    channel.queue_declare(queue='fanoutTask2')
+    channel.queue_bind(exchange='FanoutPractice',queue='fanoutTask2')
+    channel.basic_consume(queue='fanoutTask2',on_message_callback=callback,auto_ack=True)
     channel.start_consuming()
--- publisher.py
+++ publisher.py
@@ -1,14 +1,21 @@
 import pika
 from faker import Faker
 
+def reverse(x):
+  return x[::-1]
+
+
+
 fake = Faker('iw_IL')
 connection_parameters = pika.ConnectionParameters(host='localhost')
 
 with pika.BlockingConnection(connection_parameters) as connection:
     channel = connection.channel()
-    channel.queue_declare(queue='fakerQueue')
+    channel.exchange_declare(exchange='FanoutPractice',exchange_type='fanout')
+
+
     for i in range(100):
         person = fake.name()
         location = fake.city()
-        msg = f'{person} is in city {location}'
-        channel.basic_publish(exchange='',routing_key='fakerQueue',body= msg)
+        msg = f'{reverse(person)} is in city {reverse(location)}'
+        channel.basic_publish(exchange='FanoutPractice',routing_key='',body= msg)
